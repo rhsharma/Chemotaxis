a5311d2729d06bf3338f61062af06a1082b874ff
unknown <rhsharma@697-334-eowyn.sf.k12.internal>
2015-09-16 11:31:56 -0700

    Changed background and made an array with 5 elements

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 //Bacteria bob = new Bacteria();
 Bacteria [] group;
  //declare bacteria variables here   
- void setup()   
+ public void setup()   
  {     
  	size(300, 300);
  	group = new Bacteria [5];
@@ -12,7 +28,7 @@ Bacteria [] group;
 
  	//initialize bacteria variables here   
  }   
- void draw()   
+ public void draw()   
  {    
  	background(0);
  	for (int i = 0; i < group.length; i++)
@@ -32,13 +48,13 @@ Bacteria [] group;
  		myY = 150;
  		bColor = color((int)(Math.random()*200+50), (int)(Math.random()*100+50), (int)(Math.random()*200+50));
  	}
- 	void move()
+ 	public void move()
  	{
  		myX += (int)(Math.random()* 4-2);
  		myY += (int)(Math.random()* 4-2);
  	}
 
- 	void show()
+ 	public void show()
  	{
  		fill(bColor);
  		ellipse(myX, myY, 15, 15);
@@ -49,3 +65,12 @@ Bacteria [] group;
 
  	//lots of java!   
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
